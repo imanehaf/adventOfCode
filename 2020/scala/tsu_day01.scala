df7b9978480434d952cb9f4dc38946d6365a305d
imanehaf <hafmane@gmail.com>
2020-12-09 19:24:49 +0000

    init

---
+++ 2020/scala/tsu_day01.scala
@@ -0,0 +1,29 @@
+object tsu_day01 extends App {
+  val fn = System.getenv().getOrDefault("FILE_PATH", "input-tsu.txt").trim
+  pt1()
+  pt2()
+
+  def pt1(): Unit = {
+    val data = scala.io.Source.fromFile(fn).getLines.map(_.toInt).toArray
+    data.foreach { x =>
+      val y = data.filter(_ == 2020 - x)
+      if (y.nonEmpty) {
+        println(x * y(0))
+        return
+      }
+    }
+  }
+
+  def pt2(): Unit = {
+    val data = scala.io.Source.fromFile(fn).getLines.map(_.toInt).toArray
+    data.foreach { x =>
+      data.foreach { y =>
+        val z = data.filter(_ == 2020 - x - y)
+        if (z.nonEmpty) {
+          println(x * y * z(0))
+          return
+        }
+      }
+    }
+  }
+}
---
+++ 2020/scala/tsu_day09.scala
@@ -0,0 +1,44 @@
+object tsu_day09 extends App {
+  //TODO: This here ...pretends to be Scala! Don't be fooled! needs reworking...
+  val fn = System.getenv().getOrDefault("FILE_PATH", "/home/tsunade/input-tsu-d9.txt").trim
+  var data = scala.io.Source.fromFile(fn).getLines().map(_.trim.toLong).toArray
+  val pream = 25
+
+  val i = 0
+  val invalid = data(Range(i, data.length - pream - 1).find { i =>
+    val slice = data.slice(i, i + pream)
+    val k = data(i + pream)
+    !search(slice, k)
+  }.get + pream)
+  println("Uno: " + invalid)
+
+  //+=+=+=+=+=+=+=+=+
+
+  var mylist = Array(0L)
+  while (mylist.sum != invalid) {
+    var sum = 0L
+    mylist = mylist.tail
+    val y = data.takeWhile { x =>
+      sum += x
+      sum + mylist.sum <= invalid
+    }
+    data = data.drop(y.length)
+    mylist ++= y
+  }
+  println("Dos: " + (mylist.min + mylist.max))
+
+  //+=+=+=+=+=+=+=+=+
+  def search(slice: Array[Long], k: Long): Boolean = {
+    var sorts = slice.sorted
+    while (sorts.head < sorts.last) {
+      val sum = sorts.head + sorts.last
+      if (sum == k)
+        return true
+      else if (sum < k)
+        sorts = sorts.tail
+      else
+        sorts = sorts.init
+    }
+    false
+  }
+}
