0bd09554fdfbff94f65d59e13d3fa251008ab43c
imanehaf <hafmane@gmail.com>
2020-12-25 12:58:35 +0000

    d25 clean up

--- 2020/scala/tsu_day25.scala
+++ 2020/scala/tsu_day25.scala
@@ -2,14 +2,14 @@ object tsu_day25 extends App {
   val fn = System.getenv().getOrDefault("FILE_PATH", "/home/tsunade/input-tsu-d25.txt").trim
   val publicKeys = scala.io.Source.fromFile(fn).getLines().map(_.trim.toLong).toArray
 
-  val loopSize = publicKeys.map(findLoops(7, _))
-  println("Encryption key: " + encryption(publicKeys.head, loopSize.last)) // Encryption key: 7269858
+  val loopSize = publicKeys.map(encryption(7, _)._1.get)
+  println("Encryption key: " + encryption(publicKeys.head, Long.MaxValue, loopSize.last + 1)._2) // Encryption key: 7269858
   println("It's a wrap, Folks!")
 
-  def encryption(subjNum: Long, lp: Int): Long = {    Range(0, lp).foldRight(1L) { (_, v) => (v * subjNum) % 20201227L }  }
-  def findLoops(subjNum: Long, ekey: Long): Int = {
+  def encryption(subjNum: Long, ekey: Long, lp: Int = Int.MaxValue): (Option[Int], Long) = {
     var v = 1L
-    Range(1, Int.MaxValue).find { _ => v = (v * subjNum) % 20201227L
-      v == ekey }.get
+    (Range(1, lp).find { _ =>
+      v = (v * subjNum) % 20201227L
+      v == ekey    }, v)
   }
 }
